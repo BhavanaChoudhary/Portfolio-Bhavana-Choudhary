e09a44d6866b8cf9c1679f548834daee216d873f
bhavana <bhavanabc017@gmail.com>
2025-08-04 16:32:10 +0530

    .

--- portfolio/src/components/SideBar.jsx
+++ portfolio/src/components/SideBar.jsx
@@ -22,7 +22,7 @@ const VerticalOverlayBar = styled.div`
   padding: 8px 0;
 
   @media (max-width: 768px) {
-    top: 520px; /* moved lower */
+    top: 520px;
     left: 10px;
     width: 32px;
     height: 160px;
@@ -30,7 +30,7 @@ const VerticalOverlayBar = styled.div`
   }
 
   @media (max-width: 480px) {
-    top: 350px; /* moved even lower */
+    top: 350px;
     left: 8px;
     width: 30px;
     height: 150px;
@@ -119,6 +119,13 @@ export default function SideBar() {
           <path d="M12 0C5.37 0 0 5.37 0 12c0 5.3 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61-.546-1.385-1.333-1.754-1.333-1.754-1.09-.745.083-.73.083-.73 1.205.085 1.84 1.236 1.84 1.236 1.07 1.835 2.807 1.305 3.492.997.108-.775.42-1.305.763-1.605-2.665-.3-5.466-1.335-5.466-5.93 0-1.31.47-2.38 1.236-3.22-.124-.303-.536-1.523.117-3.176 0 0 1.008-.322 3.3 1.23a11.5 11.5 0 0 1 3-.405c1.02.005 2.045.138 3 .405 2.29-1.552 3.296-1.23 3.296-1.23.655 1.653.243 2.873.12 3.176.77.84 1.235 1.91 1.235 3.22 0 4.61-2.807 5.625-5.48 5.92.43.37.823 1.1.823 2.22 0 1.606-.015 2.896-.015 3.286 0 .32.216.694.825.576C20.565 21.795 24 17.295 24 12c0-6.63-5.37-12-12-12z"/>
         </svg>
         <span>Github</span>
+      </IconLink>
+
+      <IconLink href="mailto:bhavxchoudhary@gmail.com" title="Email">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
+        </svg>
+        <span>Email</span>
       </IconLink>
     </VerticalOverlayBar>
   )
